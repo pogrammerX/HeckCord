0b6262f1cac746def71d4b50e4e5439ce9eec4a6
David Gerson <84614495+pogrammerX@users.noreply.github.com>
2022-02-08 20:31:06 +0100

    Uploaded Version 1.0

---
+++ HeckCord/GlobalVars.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Discord.WebSocket;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeckCord
+{
+    public static class GlobalVars
+    {
+        public static string DiscordToken;
+        public static DiscordSocketClient client;
+    }
+}
---
+++ HeckCord/MainWindow.cs
@@ -0,0 +1,57 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeckCord
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button2Clickhandler().GetAwaiter().GetResult();
+        }
+
+        async Task Button2Clickhandler()
+        {
+            await GlobalVars.client.SetGameAsync(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            new DMChannels().Show();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            while(GlobalVars.client.CurrentUser == null)
+            {
+                Thread.Sleep(1);
+                Application.DoEvents();
+            }
+            new WebClient().DownloadFile(GlobalVars.client.CurrentUser.GetAvatarUrl(), Application.StartupPath + "\\pfp.png");
+            userpfp.Image = new Bitmap(Application.StartupPath + "\\pfp.png");
+            label1.Text = GlobalVars.client.CurrentUser.Username + "#" + GlobalVars.client.CurrentUser.Discriminator;
+        }
+    }
+}
